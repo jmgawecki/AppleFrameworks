bfb6f505271ff63d6ba0e7d9026d6a2d9dafa305
Jakub Gawecki <jmgawecki@icloud.com>
2021-01-30 17:55:01 +0000

    Initial commit

---
+++ Shared/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  Shared
+//
+//  Created by Jakub Gawecki on 30/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FrameworkGridView()
+        }
+    }
+}
---
+++ Shared/FrameworkGridView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Jakub Gawecki on 30/01/2021.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+            .font(.caption2)
+            .font(.title)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+    }
+}

2e87f8e6a9d2dad6ad58f90fba0504b1c6cf9af8
Jakub Gawecki <71930261+jmgawecki@users.noreply.github.com>
2021-02-06 15:13:01 +0000

    organisational changes

---
+++ Shared/UIKit Components/SafariView.swift
@@ -0,0 +1,20 @@
+//
+//  SafariView.swift
+//  AppleFrameworks
+//
+//  Created by Jakub Gawecki on 06/02/2021.
+//
+
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    
+    let url: URL
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {}
+}
